258ff1e35d507559ef7730b9b2ad86f360720a34
Spance <Loong@spance.dpdns.org>
2026-03-17 21:41:59 +0800

    修复session未在程序结束前关闭的问题

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Author：Spance
 # @Email: wqqd@spance.xyz
-# @Version：v1.6
+# @Version：v1.7
 # @Desc:安徽工业大学考勤系统自动签到
 
 
@@ -21,6 +21,9 @@ import asyncio
 
 """
                 更新日志
+2026年3月17日 21:39:52:
+  修复了没有在程序结束之前关闭各用户的session，若持久化运行会导致内存泄漏的问题
+
 2026年3月14日 21:57:27:
   添加了异步并发限制，现在无需担心并发太多导致学校服务器压力过大。
   修复了获取经纬度时保存的数据为str类型，导致无法偏置
@@ -60,6 +63,28 @@ class User:
     taskId: int = None
     # 当前提供的密码是否为加密之后的
     is_encrypted: int = 0
+    # 内部持有的session
+    _session = None
+    # 对外展示的session
+    @property
+    def session(self):
+        if self._session is None:
+            session = aiohttp.ClientSession(headers = {
+                'User-Agent': random.choice(UA_LIST),
+                'authorization': "Basic Zmx5c291cmNlX3dpc2VfYXBwOkRBNzg4YXNkVURqbmFzZF9mbHlzb3VyY2VfZHNkYWREQUlVaXV3cWU=",
+                'Content-Type': "application/json;charset=UTF-8",
+                'X-Requested-With': "com.tencent.mm",
+                'Origin': "https://xskq.ahut.edu.cn",
+                'Referer': f"https://xskq.ahut.edu.cn/wise/pages/ssgl/dormsign?&userId={self.student_Id}"
+            })
+            self._session = session
+        else:
+            if self.token: self._session.headers["flysource-auth"] = f"bearer {self.token}"
+        return self._session
+
+    async def close(self):
+        if self._session:
+            await self._session.close()
 
 
 ## *------------------------------------------------------* ##
@@ -304,184 +329,155 @@ async def sign_in_by_step(user: User, step: int, debug: bool = False) -> dict:
             logger.error(f'当前时间 {now_time} 未到签到时间，不进行签到')
             return {'success': False,'msg':"未到签到时间",'step': -1}
 
-    # 初始化用户session
-    async with (aiohttp.ClientSession(headers = {
-            'User-Agent': random.choice(UA_LIST),
-            'authorization': "Basic Zmx5c291cmNlX3dpc2VfYXBwOkRBNzg4YXNkVURqbmFzZF9mbHlzb3VyY2VfZHNkYWREQUlVaXV3cWU=",
-            'Content-Type': "application/json;charset=UTF-8",
-            'X-Requested-With': "com.tencent.mm",
-            'Origin': "https://xskq.ahut.edu.cn",
-            'Referer': f"https://xskq.ahut.edu.cn/wise/pages/ssgl/dormsign?&userId={user.student_Id}"
-        }) as session):
-
-        # 获取token
-        if step == 0:
-            logger.info(f"开始为 {user.student_Id} 获取token")
-            async with session.post(
-                    url=WEB_DICT["token_api"],
-                    params=generate_params(user),
-                    headers=generate_header(user)
-            ) as resp:
-                token_result = await resp.json()
-            logger.debug(f'{user.student_Id} 获取token返回信息 {token_result}')
-
-            if 'refresh_token' in token_result:
-                user.token = token_result['refresh_token']
-                user.username = token_result['userName']
-                logger.info(f"成功为 {user.username}({user.student_Id}) 获取到token")
-                logger.debug(f"{user.username}({user.student_Id}) 的token为 {user.token}")
-                return {'success': True, 'msg':'','step':step+1}
-
-            else:
-                error_desc = token_result.get('error_description','未知错误')
-                if "Bad credentials" in error_desc or "用户名或密码错误" in error_desc: error_desc = "密码错误"
-                logger.error(f"为 {user.student_Id} 获取token时，出现错误：{error_desc}")
-                return {'success': False, 'msg': error_desc, 'step': -1}
-
-
-        # 获取taskId
-        if step == 1:
-            logger.info(f"开始为 {user.username}({user.student_Id}) 获取当前签到taskId")
-            async with session.get(
-                    url=WEB_DICT['task_id_api'],
-                    headers=generate_header(user,WEB_DICT['task_id_api'])
-            ) as resp:
-                task_result = await resp.json()
-            logger.debug(f"{user.username}({user.student_Id}) 获取taskId返回信息 {task_result}")
-
-            if task_result['code'] == 200:
-                if task_result.get('data', {}).get('records', [{}])[0].get("taskId"):
-                    user.taskId = task_result.get('data').get('records')[0].get('taskId')
-                    logger.info(f"为 {user.username}({user.student_Id}) 获取到当前签到的taskId：{user.taskId}")
-                    return {'success': True, 'msg': '', 'step': step+1}
-                else:
-                    logger.error(f"{user.username}({user.student_Id}) 获取taskId时未在返回信息中解析到taskId字段，请检查{task_result}")
-                    return {'success': False, 'msg': '未在返回信息中解析到taskId字段', 'step': step}
-
-            else:
-                if (("请求未授权" in task_result.get('msg'))
-                        or ("缺失身份信息" in task_result.get('msg'))
-                        or ('鉴权失败' in task_result.get('msg'))):
-                    logger.warning(f"{user.username}({user.student_Id}) Token失效或未授权，将重试获取Token。")
-                    user.token = ''
-                    return {'success': False, 'msg': 'token失效', 'step': 0}
-                else:
-                    logger.warning(f"{user.username}({user.student_Id}) 获取taskId时出现问题：{task_result.get('msg')}")
-                    return {'success': False, 'msg': task_result.get('msg'), 'step': step}
-
-        # 获取微信接口配置
-        if step == 2:
-            logger.info(f"开始为 {user.username}({user.student_Id}) 获取微信接口配置")
-            url =WEB_DICT['auth_check_api'].format(TASK_ID=user.taskId,STUDENT_ID=user.student_Id)
-            async with session.get(
-                    url=url,
-                    headers=generate_header(user,url)
-            ) as resp:
-                auth_result = await resp.json()
-            logger.debug(f"{user.username}({user.student_Id}) 获取微信接口配置返回信息 {auth_result}")
-
-            if auth_result['code'] == 200:
-                logger.info(f"为 {user.username}({user.student_Id}) 获取微信接口配置信息成功")
+    # 获取token
+    if step == 0:
+        logger.info(f"开始为 {user.student_Id} 获取token")
+        async with user.session.post(
+                url=WEB_DICT["token_api"],
+                params=generate_params(user),
+                headers=generate_header(user)
+        ) as resp:
+            token_result = await resp.json()
+        logger.debug(f'{user.student_Id} 获取token返回信息 {token_result}')
+        if 'refresh_token' in token_result:
+            user.token = token_result['refresh_token']
+            user.username = token_result['userName']
+            logger.info(f"成功为 {user.username}({user.student_Id}) 获取到token")
+            logger.debug(f"{user.username}({user.student_Id}) 的token为 {user.token}")
+            return {'success': True, 'msg':'','step':step+1}
+        else:
+            error_desc = token_result.get('error_description','未知错误')
+            if "Bad credentials" in error_desc or "用户名或密码错误" in error_desc: error_desc = "密码错误"
+            logger.error(f"为 {user.student_Id} 获取token时，出现错误：{error_desc}")
+            return {'success': False, 'msg': error_desc, 'step': -1}
+    # 获取taskId
+    if step == 1:
+        logger.info(f"开始为 {user.username}({user.student_Id}) 获取当前签到taskId")
+        async with user.session.get(
+                url=WEB_DICT['task_id_api'],
+                headers=generate_header(user,WEB_DICT['task_id_api'])
+        ) as resp:
+            task_result = await resp.json()
+        logger.debug(f"{user.username}({user.student_Id}) 获取taskId返回信息 {task_result}")
+        if task_result['code'] == 200:
+            if task_result.get('data', {}).get('records', [{}])[0].get("taskId"):
+                user.taskId = task_result.get('data').get('records')[0].get('taskId')
+                logger.info(f"为 {user.username}({user.student_Id}) 获取到当前签到的taskId：{user.taskId}")
                 return {'success': True, 'msg': '', 'step': step+1}
-
             else:
-                if (("请求未授权" in auth_result.get('msg'))
-                        or ("缺失身份信息" in auth_result.get('msg'))
-                        or ('鉴权失败' in auth_result.get('msg'))):
-                    logger.warning(f"{user.username}({user.student_Id}) Token失效或未授权，将重试获取Token。")
-                    user.token = ''
-                    return {'success': False, 'msg': 'token失效', 'step': 0}
-                else:
-                    logger.warning(
-                        f"{user.username}({user.student_Id}) 获取微信接口配置信息时出现问题：{auth_result.get('msg')}")
-                    return {'success': False, 'msg': auth_result.get('msg'), 'step': step}
-
-        # 开启时间窗口
-        if step == 3:
-            logger.info(f"开始为 {user.username}({user.student_Id}) 开启签到时间窗口")
-            async with session.post(
-                    url=WEB_DICT["apiLog_api"],
-                    headers=generate_header(user, WEB_DICT['apiLog_api'])
-            ) as resp:
-                apiLog_result = resp
-                apiLog_text = await apiLog_result.text()
-            logger.debug(f"{user.username}({user.student_Id}) 开启签到时间窗口返回信息 {apiLog_text}")
-
-
-            if apiLog_result.status == 200:
-                logger.info(f"为 {user.username}({user.student_Id}) 开启签到时间窗口成功")
-                return {'success': True, 'msg': '', 'step': step + 1}
-
+                logger.error(f"{user.username}({user.student_Id}) 获取taskId时未在返回信息中解析到taskId字段，请检查{task_result}")
+                return {'success': False, 'msg': '未在返回信息中解析到taskId字段', 'step': step}
+        else:
+            if (("请求未授权" in task_result.get('msg'))
+                    or ("缺失身份信息" in task_result.get('msg'))
+                    or ('鉴权失败' in task_result.get('msg'))):
+                logger.warning(f"{user.username}({user.student_Id}) Token失效或未授权，将重试获取Token。")
+                user.token = ''
+                return {'success': False, 'msg': 'token失效', 'step': 0}
+            else:
+                logger.warning(f"{user.username}({user.student_Id}) 获取taskId时出现问题：{task_result.get('msg')}")
+                return {'success': False, 'msg': task_result.get('msg'), 'step': step}
+    # 获取微信接口配置
+    if step == 2:
+        logger.info(f"开始为 {user.username}({user.student_Id}) 获取微信接口配置")
+        url =WEB_DICT['auth_check_api'].format(TASK_ID=user.taskId,STUDENT_ID=user.student_Id)
+        async with user.session.get(
+                url=url,
+                headers=generate_header(user,url)
+        ) as resp:
+            auth_result = await resp.json()
+        logger.debug(f"{user.username}({user.student_Id}) 获取微信接口配置返回信息 {auth_result}")
+        if auth_result['code'] == 200:
+            logger.info(f"为 {user.username}({user.student_Id}) 获取微信接口配置信息成功")
+            return {'success': True, 'msg': '', 'step': step+1}
+        else:
+            if (("请求未授权" in auth_result.get('msg'))
+                    or ("缺失身份信息" in auth_result.get('msg'))
+                    or ('鉴权失败' in auth_result.get('msg'))):
+                logger.warning(f"{user.username}({user.student_Id}) Token失效或未授权，将重试获取Token。")
+                user.token = ''
+                return {'success': False, 'msg': 'token失效', 'step': 0}
             else:
                 logger.warning(
-                    f"{user.username}({user.student_Id}) 开启签到时间窗口时出现问题")
-                return {'success': False, 'msg': "开启签到时间窗口时出现问题", 'step': step}
-
-        # 获取签到的指定位置
-        if step == 4:
-            logger.info(f"开始获取 {user.username}({user.student_Id}) 签到位置")
-            url = WEB_DICT['get_location_api'].format(TASK_ID=user.taskId,date_str=datetime.now().strftime('%Y-%m-%d'))
-            async with session.get(
-                    url,
-                    headers=generate_header(user, url)
-            ) as resp:
-                location_result = await resp.json()
-            logger.debug(f"{user.username}({user.student_Id}) 获取签到位置返回信息 {location_result}")
-
-            if location_result['code'] == 200:
-                user.latitude=float(location_result['data'].get('dormitoryRegisterVO',{}).get('locationLat'))
-                user.longitude=float(location_result['data'].get('dormitoryRegisterVO',{}).get('locationLng'))
-                logger.info(f"为 {user.username}({user.student_Id}) 获取签到位置成功")
-                return {'success': True, 'msg': '', 'step': step+1}
+                    f"{user.username}({user.student_Id}) 获取微信接口配置信息时出现问题：{auth_result.get('msg')}")
+                return {'success': False, 'msg': auth_result.get('msg'), 'step': step}
+    # 开启时间窗口
+    if step == 3:
+        logger.info(f"开始为 {user.username}({user.student_Id}) 开启签到时间窗口")
+        async with user.session.post(
+                url=WEB_DICT["apiLog_api"],
+                headers=generate_header(user, WEB_DICT['apiLog_api'])
+        ) as resp:
+            apiLog_result = resp
+            apiLog_text = await apiLog_result.text()
+        logger.debug(f"{user.username}({user.student_Id}) 开启签到时间窗口返回信息 {apiLog_text}")
+        if apiLog_result.status == 200:
+            logger.info(f"为 {user.username}({user.student_Id}) 开启签到时间窗口成功")
+            return {'success': True, 'msg': '', 'step': step + 1}
+        else:
+            logger.warning(
+                f"{user.username}({user.student_Id}) 开启签到时间窗口时出现问题")
+            return {'success': False, 'msg': "开启签到时间窗口时出现问题", 'step': step}
+    # 获取签到的指定位置
+    if step == 4:
+        logger.info(f"开始获取 {user.username}({user.student_Id}) 签到位置")
+        url = WEB_DICT['get_location_api'].format(TASK_ID=user.taskId,date_str=datetime.now().strftime('%Y-%m-%d'))
+        async with user.session.get(
+                url,
+                headers=generate_header(user, url)
+        ) as resp:
+            location_result = await resp.json()
+        logger.debug(f"{user.username}({user.student_Id}) 获取签到位置返回信息 {location_result}")
+        if location_result['code'] == 200:
+            user.latitude=float(location_result['data'].get('dormitoryRegisterVO',{}).get('locationLat'))
+            user.longitude=float(location_result['data'].get('dormitoryRegisterVO',{}).get('locationLng'))
+            logger.info(f"为 {user.username}({user.student_Id}) 获取签到位置成功")
+            return {'success': True, 'msg': '', 'step': step+1}
+        else:
+            if (("请求未授权" in location_result.get('msg'))
+                    or ("缺失身份信息" in location_result.get('msg'))
+                    or ('鉴权失败' in location_result.get('msg'))):
+                logger.warning(f"{user.username}({user.student_Id}) Token失效或未授权，将重试获取Token。")
+                user.token = ''
+                return {'success': False, 'msg': 'token失效', 'step': 0}
             else:
-                if (("请求未授权" in location_result.get('msg'))
-                        or ("缺失身份信息" in location_result.get('msg'))
-                        or ('鉴权失败' in location_result.get('msg'))):
-                    logger.warning(f"{user.username}({user.student_Id}) Token失效或未授权，将重试获取Token。")
-                    user.token = ''
-                    return {'success': False, 'msg': 'token失效', 'step': 0}
-                else:
-                    user.longitude=31.668
-                    user.latitude=118.227
-                    logger.warning(f"未能为 {user.username}({user.student_Id}) 获取签到位置，已使用默认位置托底")
-                    return {"success":True,"msg":"","step":step+1}
-
-
-        # 进行晚寝签到
-        if step == 5:
-            logger.info(f"开始为 {user.username}({user.student_Id}) 晚寝签到")
-            async with session.post(
-                    url=WEB_DICT["sign_in_api"],
-                    json=generate_data(user),
-                    headers=generate_header(user,WEB_DICT['sign_in_api'])
-            ) as resp:
-                sign_in_result = await resp.json()
-            logger.debug(f"{user.username}({user.student_Id}) 晚寝签到返回信息 {sign_in_result}")
-
-            if sign_in_result['code'] == 200 or '您今天已完成签到' in sign_in_result['msg']:
-                logger.info(f"为 {user.username}({user.student_Id}) 晚寝签到成功")
-                return {'success': True, 'msg': '', 'step': step + 1}
-
+                user.longitude=31.668
+                user.latitude=118.227
+                logger.warning(f"未能为 {user.username}({user.student_Id}) 获取签到位置，已使用默认位置托底")
+                return {"success":True,"msg":"","step":step+1}
+    # 进行晚寝签到
+    if step == 5:
+        logger.info(f"开始为 {user.username}({user.student_Id}) 晚寝签到")
+        async with user.session.post(
+                url=WEB_DICT["sign_in_api"],
+                json=generate_data(user),
+                headers=generate_header(user,WEB_DICT['sign_in_api'])
+        ) as resp:
+            sign_in_result = await resp.json()
+        logger.debug(f"{user.username}({user.student_Id}) 晚寝签到返回信息 {sign_in_result}")
+        if sign_in_result['code'] == 200 or '您今天已完成签到' in sign_in_result['msg']:
+            logger.info(f"为 {user.username}({user.student_Id}) 晚寝签到成功")
+            return {'success': True, 'msg': '', 'step': step + 1}
+        else:
+            if (("请求未授权" in sign_in_result.get('msg'))
+                    or ("缺失身份信息" in sign_in_result.get('msg'))
+                    or ('鉴权失败' in sign_in_result.get('msg'))):
+                logger.warning(f"{user.username}({user.student_Id}) Token失效或未授权，将重试获取Token。")
+                user.token = ''
+                return {'success': False, 'msg': 'token失效', 'step': 0}
             else:
-                if (("请求未授权" in sign_in_result.get('msg'))
-                        or ("缺失身份信息" in sign_in_result.get('msg'))
-                        or ('鉴权失败' in sign_in_result.get('msg'))):
-                    logger.warning(f"{user.username}({user.student_Id}) Token失效或未授权，将重试获取Token。")
-                    user.token = ''
-                    return {'success': False, 'msg': 'token失效', 'step': 0}
-                else:
-                    if '未到签到时间！' in sign_in_result.get('msg'):
-                        logger.warning(
-                            f"因当前时间{get_time()['time']}未到签到时间，{user.username}({user.student_Id}) 签到失败")
-                        return {'success': False, 'msg': sign_in_result.get('msg'), 'step': -1}
+                if '未到签到时间！' in sign_in_result.get('msg'):
                     logger.warning(
-                        f"{user.username}({user.student_Id}) 晚寝签到时出现问题：{sign_in_result.get('msg')}")
-                    return {'success': False, 'msg': sign_in_result.get('msg'), 'step': step}
+                        f"因当前时间{get_time()['time']}未到签到时间，{user.username}({user.student_Id}) 签到失败")
+                    return {'success': False, 'msg': sign_in_result.get('msg'), 'step': -1}
+                logger.warning(
+                    f"{user.username}({user.student_Id}) 晚寝签到时出现问题：{sign_in_result.get('msg')}")
+                return {'success': False, 'msg': sign_in_result.get('msg'), 'step': step}
 
     # 未知情况或传入的step错误
-        else:
-            logger.debug(f"出现未知错误，当前参数为：user={user.student_Id},step={step}")
-            return {'success': False, 'msg': '', 'step': -1}
+    else:
+        logger.debug(f"出现未知错误，当前参数为：user={user.student_Id},step={step}")
+        return {'success': False, 'msg': '', 'step': -1}
 
 
 async def sign_in(user: User, debug: bool = False):
@@ -525,6 +521,7 @@ async def main():
 
     async_results = await asyncio.gather(
         *(limited_sign_in(u) for u in USER_LIST))
+    await asyncio.gather(*[user.close() for user in USER_LIST])
     return {u.student_Id:result for u,result in zip(USER_LIST,async_results)}
 
 
